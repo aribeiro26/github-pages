17140026188ea3793101526ae7c9bc7958fe0c77
aribeiro26 <73800428+aribeiro26@users.noreply.github.com>
2021-09-14 03:14:31 -0300

    gitpages

---
+++ email.php
@@ -0,0 +1,25 @@
+<?php
+if(isset(&_POST['email']) && !empty(&_POST['email']){
+
+&nome = addcslashes(&_POST['name']);
+&email = addcslashes(&_POST['email']);
+&mensagem = addcslashes(&_POST['menssage']);
+
+$to = "alexetec.ribeiro@gmail.com";
+$subject = "Contato - Luz Inga";
+$body = "Nome: ".$nome. "\r\n".
+        "Email: ".$email. "\r\n".
+        "Mensagem: ".$mensagem;
+$header = "From: alex.ribeiro.94@hotmail.com"."\r\n".
+            ."Reply-To:".$email."\r\n".
+            ."X=Mailer:PHP".phpversion();
+
+}
+if(mail($to.$subject,$body,$header)){
+    echo("Email enviado com sucesso!");
+}else{
+    echo("O email não pode ser enviado");
+}
+}
+
+?>
