5fc1f9ca98431b27dd6cd111a142d7d5e06536cc
aribeiro26 <73800428+aribeiro26@users.noreply.github.com>
2021-09-14 03:23:08 -0300

    correcao

--- email.php
+++ email.php
@@ -1,9 +1,9 @@
 <?php
-if(isset(&_POST['email']) && !empty(&_POST['email']){
+if(isset($_POST['email']) && !empty($_POST['email']){
 
-&nome = addcslashes(&_POST['name']);
-&email = addcslashes(&_POST['email']);
-&mensagem = addcslashes(&_POST['menssage']);
+&nome = addcslashes($_POST['name']);
+&email = addcslashes($_POST['email']);
+&mensagem = addcslashes($_POST['menssage']);
 
 $to = "alexetec.ribeiro@gmail.com";
 $subject = "Contato - Luz Inga";
@@ -11,8 +11,8 @@ $body = "Nome: ".$nome. "\r\n".
         "Email: ".$email. "\r\n".
         "Mensagem: ".$mensagem;
 $header = "From: alex.ribeiro.94@hotmail.com"."\r\n".
-            ."Reply-To:".$email."\r\n".
-            ."X=Mailer:PHP".phpversion();
+            "Reply-To:".$email."\r\n".
+            "X=Mailer:PHP".phpversion();
 
 }
 if(mail($to.$subject,$body,$header)){
